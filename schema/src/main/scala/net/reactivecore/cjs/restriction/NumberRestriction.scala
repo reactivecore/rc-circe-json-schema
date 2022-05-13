a2923b9972ccf5ccd80a287293a6f0551a44ce81
Norbert Schultz <norbert.schultz@reactivecore.de>
2022-05-13 15:53:05 +0200

    This seems to work

--- schema/src/main/scala/net/reactivecore/cjs/restriction/NumberRestriction.scala
+++ schema/src/main/scala/net/reactivecore/cjs/restriction/NumberRestriction.scala
@@ -3,15 +3,22 @@ package net.reactivecore.cjs.restriction
 import io.circe.generic.semiauto
 import io.circe.{Codec, Json}
 import net.reactivecore.cjs.util.Codecs
-import net.reactivecore.cjs.validator.provider.ValidationProvider
+import net.reactivecore.cjs.validator.number.{
+  ExclusiveMaximumValidator,
+  ExclusiveMinimumValidator,
+  MaximumValidator,
+  MinimumValidator,
+  MultipleOfValidator
+}
+import net.reactivecore.cjs.validator.provider.{SequenceValidationProvider, ValidationProvider}
 import net.reactivecore.cjs.validator.{ValidationResult, Validator, Violation}
 
 case class NumberRestriction(
-    minimum: Option[BigDecimal] = None,
-    exclusiveMinimum: Option[BigDecimal] = None,
-    maximum: Option[BigDecimal] = None,
-    exclusiveMaximum: Option[BigDecimal] = None,
-    multipleOf: Option[BigDecimal] = None
+    minimum: Option[SingleRestriction[BigDecimal, MinimumValidator]] = None,
+    exclusiveMinimum: Option[SingleRestriction[BigDecimal, ExclusiveMinimumValidator]] = None,
+    maximum: Option[SingleRestriction[BigDecimal, MaximumValidator]] = None,
+    exclusiveMaximum: Option[SingleRestriction[BigDecimal, ExclusiveMaximumValidator]] = None,
+    multipleOf: Option[SingleRestriction[BigDecimal, MultipleOfValidator]] = None
 ) {
   def isEmpty: Boolean = this == NumberRestriction()
 }
@@ -19,52 +26,6 @@ case class NumberRestriction(
 object NumberRestriction {
   implicit lazy val codec: Codec.AsObject[NumberRestriction] = Codecs.withoutNulls(semiauto.deriveCodec)
 
-  implicit lazy val validationProvider: ValidationProvider[NumberRestriction] = ValidationProvider.instance {
-    numberRestriction =>
-      val numberChecks = buildNumberChecks(numberRestriction)
-
-      if (numberRestriction.isEmpty) {
-        Validator.success
-      } else
-        Validator.contextFree { json =>
-          {
-            ValidationResult(
-              json
-                .as[BigDecimal]
-                .toSeq
-                .flatMap { n =>
-                  numberChecks(n).violations
-                }
-            )
-          }
-        }
-  }
-
-  private def buildNumberChecks(
-      numberRestriction: NumberRestriction
-  ): BigDecimal => ValidationResult = {
-    def build(
-        restriction: NumberRestriction => Option[BigDecimal],
-        name: String,
-        comparer: (BigDecimal, BigDecimal) => Boolean
-    ): Option[BigDecimal => ValidationResult] = {
-      restriction(numberRestriction).map { restriction => input =>
-        if (comparer(restriction, input)) {
-          ValidationResult.success
-        } else {
-          ValidationResult.violation(Json.fromBigDecimal(input), name)
-        }
-      }
-    }
-
-    val checks = Seq(
-      build(_.minimum, "minimum", _ <= _),
-      build(_.maximum, "maximum", _ >= _),
-      build(_.exclusiveMinimum, "exclusiveMinimum", _ < _),
-      build(_.exclusiveMaximum, "exclusiveMaximum", _ > _),
-      build(_.multipleOf, "multipleOf", (r, n) => n % r == 0)
-    ).flatten
-
-    input => ValidationResult(checks.flatMap(_.apply(input).violations))
-  }
+  implicit lazy val validationProvider: ValidationProvider[NumberRestriction] =
+    SequenceValidationProvider[NumberRestriction]
 }
